33c4b00db11cad86cb6ca25118622b2d64626396
吴继信 <11297529+wjx4r@user.noreply.gitee.com>
2022-08-28 15:56:11 +0800

    bibb

--- src/main/java/com/wjx/the_golden_autumn/block/BlockQiuxiBiologicalActivityReducer.java
+++ src/main/java/com/wjx/the_golden_autumn/block/BlockQiuxiBiologicalActivityReducer.java
@@ -1,23 +1,32 @@
 package com.wjx.the_golden_autumn.block;
 
 import com.sun.javafx.scene.traversal.Direction;
+import com.wjx.the_golden_autumn.TheGoldenAutumnMod;
 import com.wjx.the_golden_autumn.event.updatecolor;
 import com.wjx.the_golden_autumn.init.blockinit;
 import com.wjx.the_golden_autumn.init.iteminit;
 import com.wjx.the_golden_autumn.lib.LazyOptional;
+import com.wjx.the_golden_autumn.util.interfaces.IHasModel;
 import javafx.beans.property.BooleanProperty;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerChest;
 import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -28,9 +37,7 @@ import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityLockable;
 import net.minecraft.tileentity.TileEntityLockableLoot;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ITickable;
-import net.minecraft.util.NonNullList;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
@@ -255,7 +262,9 @@ public class BlockQiuxiBiologicalActivityReducer {
             }
         }
     }
-    public static class CustomBlock extends Block implements ITileEntityProvider {
+    public static class CustomBlock extends Block implements ITileEntityProvider, IHasModel {
+
+        public static final PropertyDirection FACING = BlockHorizontal.FACING;
 
         public CustomBlock() {
             super(Material.IRON);
@@ -264,7 +273,11 @@ public class BlockQiuxiBiologicalActivityReducer {
             setCreativeTab(TabGold);
             iteminit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
             blockinit.Blocks.add(this);
-
+            setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+        }
+        @Override
+        public void registerModels(){
+            TheGoldenAutumnMod.proxy.registerItemRenderer(Item.getItemFromBlock(this),0,"inventory");
         }
 
         @Override
@@ -330,6 +343,72 @@ public class BlockQiuxiBiologicalActivityReducer {
         @Override
         public boolean hasTileEntity(IBlockState state) {
             return true;
+        }
+
+        @Override
+        public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+            IBlockState north = worldIn.getBlockState(pos.north());
+            IBlockState south = worldIn.getBlockState(pos.south());
+            IBlockState west = worldIn.getBlockState(pos.west());
+            IBlockState east = worldIn.getBlockState(pos.east());
+            EnumFacing face = state.getValue(FACING);
+            if (face == EnumFacing.NORTH && north.isFullBlock() && !south.isFullBlock()) face = EnumFacing.SOUTH;
+            else if(face ==EnumFacing.SOUTH &&south.isFullBlock()&&!north.isFullBlock()) face = EnumFacing.NORTH;
+            else if(face ==EnumFacing.WEST &&west.isFullBlock()&&!east.isFullBlock()) face = EnumFacing.EAST;
+            else if(face ==EnumFacing.EAST &&east.isFullBlock()&&!west.isFullBlock()) face = EnumFacing.WEST;
+
+            worldIn.setBlockState(pos,state.withProperty(FACING,face),2);
+        }
+
+        @Override
+        public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+            return this.getDefaultState().withProperty(FACING,placer.getHorizontalFacing().getOpposite());
+        }
+
+        @Override
+        public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+        {
+            worldIn.setBlockState(pos, this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
+        }
+
+        @Override
+        public EnumBlockRenderType getRenderType(IBlockState state)
+        {
+            return EnumBlockRenderType.MODEL;
+        }
+
+        @Override
+        public IBlockState withRotation(IBlockState state, Rotation rot)
+        {
+            return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
+        }
+
+        @Override
+        public IBlockState withMirror(IBlockState state, Mirror mirrorIn)
+        {
+            return state.withRotation(mirrorIn.toRotation((EnumFacing)state.getValue(FACING)));
+        }
+
+
+
+        @Override
+        protected BlockStateContainer createBlockState()
+        {
+            return new BlockStateContainer(this, new IProperty[] {FACING});
+        }
+
+        @Override
+        public IBlockState getStateFromMeta(int meta)
+        {
+            EnumFacing facing = EnumFacing.getFront(meta);
+            if(facing.getAxis() == EnumFacing.Axis.Y) facing = EnumFacing.NORTH;
+            return this.getDefaultState().withProperty(FACING, facing);
+        }
+
+        @Override
+        public int getMetaFromState(IBlockState state)
+        {
+            return state.getValue(FACING).getIndex();
         }
     }
 }
