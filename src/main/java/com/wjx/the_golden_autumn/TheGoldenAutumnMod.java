a6302174c3a3bbf6b709b5b0c0fea6846820b757
WJXhhh <72961648+WJXhhh@users.noreply.github.com>
2024-02-03 13:13:19 +0800

    change version

--- src/main/java/com/wjx/the_golden_autumn/TheGoldenAutumnMod.java
+++ src/main/java/com/wjx/the_golden_autumn/TheGoldenAutumnMod.java
@@ -44,7 +44,7 @@ public class TheGoldenAutumnMod
 {
     public static final String MODID = "the_golden_autumn";
     public static final String NAME = "The Golden Autumn";
-    public static final String VERSION = "12.0.3.7";
+    public static final String VERSION = "12.0.3.9";
 
     public static final String CLIENT = "com.wjx.the_golden_autumn.proxy.ClientProxy";
     public static final String COMMON = "com.wjx.the_golden_autumn.proxy.CommonProxy";
