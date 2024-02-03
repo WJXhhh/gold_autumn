5fb8b6a9053c892562fd635a91d07587a523c88a
WJXhhh <miladuo43@163.com>
2024-02-03 12:55:51 +0800

    12.3.0.7 fix Jaoshingan skill,fix metoritesword

--- src/main/java/com/wjx/the_golden_autumn/block/BlockQiu_Rose.java
+++ src/main/java/com/wjx/the_golden_autumn/block/BlockQiu_Rose.java
@@ -24,6 +24,8 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -43,6 +45,7 @@ public class BlockQiu_Rose extends BlockFlower implements IHasModel, ITileEntity
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
         super.addInformation(stack, player, tooltip, advanced);
         tooltip.add(I18n.format("info.qiurose"));
